cebd2cd3d593583b4644910c0ed0eed3f3127739
PhatNH4 <phatnh4@vng.com.vn>
2024-11-29 17:03:01 +0700

    [ZMA-1038] keep background image

--- src/components/utilities/Utinities.tsx
+++ src/components/utilities/Utinities.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
+import Background from "@assets/background.png";
 import UtinityItem, { UtinityItemProps } from "./UtilityItem";
 
 interface UtinitiesProps {
@@ -8,7 +9,8 @@ interface UtinitiesProps {
 }
 
 const UtinitiesWrapper = styled.div`
-    ${tw`flex flex-row flex-wrap justify-between bg-ui_bg`};
+    ${tw`flex flex-row flex-wrap justify-between bg-ui_bg bg-center bg-no-repeat`};
+    background-image: url(${Background});
     padding: 16px;
     padding-top: 24px;
 `;
